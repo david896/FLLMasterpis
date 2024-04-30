d49aca6a10aecdb4360b99e6ea5052063ea24c95
david896 <68560407+david896@users.noreply.github.com>
2024-04-30 19:05:58 +0300

    Update Setup24_ST7789.h

--- Setup24_ST7789.h
+++ Setup24_ST7789.h
@@ -28,7 +28,7 @@
 
 // For NodeMCU - use pin numbers in the form PIN_Dx where Dx is the NodeMCU pin designation
 #define TFT_CS   -1      // Define as not used
-#define TFT_DC   26  // Data Command control pin
+#define TFT_DC   5  // Data Command control pin
 #define TFT_RST  19// TFT reset pin (could connect to NodeMCU RST, see next line)
 //#define TFT_RST  -1    // TFT reset pin connect to NodeMCU RST, must also then add 10K pull down to TFT SCK
 
